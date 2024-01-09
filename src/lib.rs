1f7dccff54af0b71c639cdb63510014b59285d73
eri <eri@inventati.org>
2024-01-09 21:40:33 +0100

    feat: caché persistente entre execucións 🌳

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use db::Conexion;
 use tokio::sync::RwLock;
+use tracing_subscriber::{fmt::time::OffsetTime, layer::SubscriberExt, util::SubscriberInitExt};
 
 mod db;
 pub mod routes;
@@ -17,3 +18,19 @@ pub struct AppState {
 
 #[derive(Clone, Default)]
 pub struct SharedState(Arc<RwLock<AppState>>);
+
+// Logs
+
+pub fn init_tracing() {
+    let timer = time::format_description::parse("[hour]:[minute]:[second]").unwrap_or_default();
+    let offset = time::UtcOffset::current_local_offset().unwrap_or(time::UtcOffset::UTC);
+    let timer = OffsetTime::new(offset, timer);
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "cinemazarelos=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer().compact().with_timer(timer))
+        .init();
+}
