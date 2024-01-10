720849a448fad68e6b7c67990e83f296830a2880
eri <eri@inventati.org>
2024-01-10 14:13:12 +0100

    chore: preprare for rebase 📦

--- app/(components)/card/card.tsx
+++
@@ -1,9 +0,0 @@
-import style from "./card.module.css"
-
-const Card = ({children} : {children: React.ReactNode}) => {
-    return (<div className={style.card}>
-        {children}
-    </div>)
-}
-
-export default Card
--- app/(lib)/analytics.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-// privacy friendly analytics (no cookies, no user data collected)
-import { Analytics as VercelAnalytics } from "@vercel/analytics/react"
-
-const Analytics = () => <VercelAnalytics/>
-
-export default Analytics
